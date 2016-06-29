690abbca2d43201dd90f247a64f11be79e30f67c
Heath Garvey <heathgarvey@gmail.com>
2016-06-29 10:24:04 +1000

    Connected the app to server data)

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,25 +1,9 @@
 const app = angular.module('myApp', []);
-app.controller('AppCtrl', function($scope) {
-    console.log("It's your boy bangs");
+app.controller('AppCtrl', function($scope, $http) {
+    console.log("I'm the Fat Controller");
 
-  person1 = {
-    name: 'Tim',
-    email: 'lads@gmail.com',
-    number: '0561 219 3181'
-  };
-
-  person2 = {
-    name: 'Cheryl',
-    email: 'cherylnotcherub@gmail.com',
-    number: '1800 981 381'
-  };
-
-  person3 = {
-    name: 'Jerry',
-    email: 'classicgag@hotmail.com',
-    number: '333 333 981'
-  };
-
-  var contactlist = [person1, person2, person3];
-  $scope.contactlist = contactlist;
+  $http.get('/contactlist').success(function(response) {
+    console.log("I have the contactlist data");
+    $scope.contactlist = response;
+  });
 });﻿
--- server.js
+++ server.js
@@ -3,6 +3,31 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactlist', function(req, res) {
+  console.log("I got a get ... request");
+
+    person1 = {
+      name: 'Tim',
+      email: 'lads@gmail.com',
+      number: '0561 219 3181'
+    };
+
+    person2 = {
+      name: 'Cheryl',
+      email: 'cherylnotcherub@gmail.com',
+      number: '1800 981 381'
+    };
+
+    person3 = {
+      name: 'Jerry',
+      email: 'classicgag@hotmail.com',
+      number: '333 333 981'
+    };
+
+    var contactlist = [person1, person2, person3];
+    res.json(contactlist);
+});
+
 app.listen(3000, function () {
   console.log('Tuning in to port 3000!');
 });
